48c77c5cdcea344d533d34b669eefacf676690d3
Chris Yu <luxtars.dev@gmail.com>
2019-07-18 15:53:15 -0400

    Update Home page skeleton

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div>Home Page</div>
+      </div>
+    );
+  }
+}
+
+export default Home;
