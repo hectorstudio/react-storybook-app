359bdecde528eb76d6895e04a0d456f3e1cabb4f
Chris Yu <luxtars.dev@gmail.com>
2019-07-18 15:24:17 -0400

    Add Motserrat Font

--- src/AppStyle.js
+++ src/AppStyle.js
@@ -1,7 +1,39 @@
 import styled from 'styled-components';
 import { palette, font } from 'styled-theme';
 
+import normalFont from './assets/font/Montserrat-Regular.otf';
+import lightFont from './assets/font/Montserrat-Light.otf';
+import mediumFont from './assets/font/Montserrat-Medium.otf';
+import boldFont from './assets/font/Montserrat-Bold.otf';
+import italicFont from './assets/font/Montserrat-LightItalic.otf';
+
 const AppHolder = styled.div`
+  @font-face {
+    font-family: Montserrat;
+    src: url(${normalFont});
+    font-weight: normal;
+  }
+  @font-face {
+    font-family: Montserrat;
+    src: url(${lightFont});
+    font-weight: lighter;
+  }
+  @font-face {
+    font-family: Montserrat;
+    src: url(${boldFont});
+    font-weight: bold;
+  }
+  @font-face {
+    font-family: Montserrat;
+    src: url(${mediumFont});
+    font-weight: medium;
+  }
+  @font-face {
+    font-family: Montserrat;
+    src: url(${italicFont});
+    font-weight: italic;
+  }
+
   font-family: ${font('primary', 0)};
 
   h1,
@@ -36,20 +68,6 @@ const AppHolder = styled.div`
 
   .ant-row > div {
     padding: 0;
-  }
-
-  .isoLeftRightComponent {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-  }
-
-  .isoCenterComponent {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
   }
 `;
 
