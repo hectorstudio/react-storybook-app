7cbc25acf5ce75cdcec006411a91cc671ff86794
Chris Yu <luxtars.dev@gmail.com>
2019-07-24 06:18:09 -0400

    Add Themeprovider to storybook

--- src/components/uielements/tabs/tabs.stories.js
+++ src/components/uielements/tabs/tabs.stories.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
 
+import AppHolder from '../../../AppStyle';
+import { defaultTheme } from '../../../settings';
 import Tabs from './tabs';
 
 const { TabPane } = Tabs;
 
 storiesOf('Components/Tabs', module).add('default', () => {
   return (
-    <Tabs defaultActivateKey="1">
-      <TabPane tab="Swap" key="1">
-        Content of Tab Pane 1
-      </TabPane>
-      <TabPane tab="Pools" key="2">
-        Content of Tab Pane 2
-      </TabPane>
-      <TabPane tab="Trade" key="3">
-        Content of Tab Pane 3
-      </TabPane>
-    </Tabs>
+    <ThemeProvider theme={defaultTheme}>
+      <AppHolder>
+        <Tabs defaultActivateKey="1">
+          <TabPane tab="Swap" key="1">
+            Content of Tab Pane 1
+          </TabPane>
+          <TabPane tab="Pools" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+          <TabPane tab="Trade" key="3">
+            Content of Tab Pane 3
+          </TabPane>
+        </Tabs>
+      </AppHolder>
+    </ThemeProvider>
   );
 });
