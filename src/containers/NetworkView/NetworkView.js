8bbe5464732a2b10908f3fcf41aac99d4359e49e
Chris Yu <luxtars.dev@gmail.com>
2019-07-26 16:52:13 -0400

    Add blocks to network view

--- src/containers/NetworkView/NetworkView.js
+++ src/containers/NetworkView/NetworkView.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 
 import { ContentWrapper } from './NetworkView.style';
-import { stats } from './data';
 import StatusGroup from '../../components/uielements/statusGroup';
+import { stats, tableData, columns } from './data';
+import Table from '../../components/uielements/table';
+import Label from '../../components/uielements/label';
 
 class NetworkView extends Component {
   render() {
+    const tableActionColumn = {
+      title: '',
+      dataIndex: '',
+      key: '',
+      render: () => (
+        <Label className="action" color="primary" weight="bold">
+          view
+        </Label>
+      ),
+    };
+    const tableColumns = columns;
+    tableColumns.push(tableActionColumn);
+
     return (
       <ContentWrapper>
         <Row>
@@ -17,6 +32,17 @@ class NetworkView extends Component {
           <Col span="12">
             <StatusGroup title="threshold addresses" status={stats.threshold} />
           </Col>
+        </Row>
+        <Row className="network-blocks">
+          <Label
+            className="group-title"
+            color="normal"
+            size="big"
+            weight="bold"
+          >
+            BLOCKS
+          </Label>
+          <Table dataSource={tableData} columns={tableColumns} />
         </Row>
       </ContentWrapper>
     );
--- src/containers/NetworkView/NetworkView.style.js
+++ src/containers/NetworkView/NetworkView.style.js
@@ -6,4 +6,16 @@ export const ContentWrapper = styled(ContentView)`
   & > .ant-row {
     display: flex;
   }
+
+  .network-blocks {
+    display: flex;
+    flex-direction: column;
+    margin-top: 15px;
+  }
+
+  .ant-table {
+    .label-wrapper {
+      padding: 0;
+    }
+  }
 `;
