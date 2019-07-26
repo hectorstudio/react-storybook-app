c6af200ce31fe0c6031b771ca4075576b339a573
hectorstudio <david.liu987@hotmail.com>
2019-07-26 18:55:43 -0400

    Add layout to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import { configure } from '@storybook/react';
 import { addDecorator } from '@storybook/react/dist/client/preview';
 import { withKnobs } from '@storybook/addon-knobs';
@@ -6,6 +9,11 @@ import 'antd/dist/antd.css';
 import './global.css';
 
 addDecorator(withKnobs);
+addDecorator(story => (
+  <Router>
+    <Route path="/" component={() => story()} />
+  </Router>
+));
 
 const req = require.context('../src', true, /\.stories\.js$/);
 
