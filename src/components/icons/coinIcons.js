31e8c6660845fe9375f08ff83ba36ff87ae97a31
Chris Yu <luxtars.dev@gmail.com>
2019-07-26 18:17:26 -0400

    Add coinIcon component

---
+++ src/components/icons/coinIcons.js
@@ -0,0 +1,15 @@
+export const bnbIcon = require('../../assets/SVG/coin-bnb.svg');
+export const boltIcon = require('../../assets/SVG/coin-bolt.svg');
+export const runeIcon = require('../../assets/SVG/coin-rune.svg');
+export const ankrIcon = require('../../assets/SVG/coin-ankr.svg');
+export const ftmIcon = require('../../assets/SVG/coin-ftm.svg');
+export const tomoIcon = require('../../assets/SVG/coin-tomob.svg');
+
+export const coinIconGroup = {
+  bnb: bnbIcon,
+  bolt: boltIcon,
+  rune: runeIcon,
+  ankr: ankrIcon,
+  ftm: ftmIcon,
+  tomo: tomoIcon,
+};
