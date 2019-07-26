3b7efc816d04d6316065cd1144054395973146ae
hectorstudio <david.liu987@hotmail.com>
2019-07-25 20:31:27 -0400

    Update script

--- src/components/uielements/statusGroup/statusGroup.stories.js
+++ src/components/uielements/statusGroup/statusGroup.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
-import { Input } from 'antd';
 
 import AppHolder from '../../../AppStyle';
 import { defaultTheme } from '../../../settings';
