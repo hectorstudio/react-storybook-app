b871e186d9c9b740435812251f0c6d31daa48671
hectorstudio <david.liu987@hotmail.com>
2019-07-26 18:43:39 -0400

    Update coin component with over props

--- src/components/uielements/coins/coin/coin.style.js
+++ src/components/uielements/coins/coin/coin.style.js
@@ -7,3 +7,21 @@ export const CoinWrapper = styled.div`
   padding: ${props => (props.size === 'small' ? '6px' : '8px')}
     ${props => (props.size === 'small' ? '6px' : '8px')};
 `;
+
+export const CoinsWrapper = styled.div`
+  position: relative;
+  .coin-bottom,
+  .coin-over {
+    position: relative;
+    display: inline-block;
+    border-radius: 50%;
+    box-shadow: 0px 0px 4px 0.5px ${palette('secondary', 2)};
+    background-color: #fff;
+    padding: ${props => (props.size === 'small' ? '6px' : '8px')}
+      ${props => (props.size === 'small' ? '6px' : '8px')};
+  }
+
+  .coin-over {
+    left: ${props => (props.size === 'small' ? '-12px' : '-15px')};
+  }
+`;
