70600dea0aab0188628facbb96fd9089a0770c5c
hectorstudio <david.liu987@hotmail.com>
2019-07-28 21:14:52 -0400

    Add left padding

--- src/containers/WalletView/WalletView.style.js
+++ src/containers/WalletView/WalletView.style.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 import ViewPanel from '../../components/viewPanel';
 
 export const WalletViewWrapper = styled(ViewPanel)`
-  .asset-title-label {
+  .asset-title-label,
+  button {
     padding-left: 20px;
   }
 `;
