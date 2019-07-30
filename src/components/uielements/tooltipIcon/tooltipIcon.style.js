dbd2d12799b6c4115f6d33f7ab769ea0062ef203
Chris Yu <luxtars.dev@gmail.com>
2019-07-30 18:16:07 -0400

    Add tooltip icon component

---
+++ src/components/uielements/tooltipIcon/tooltipIcon.style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { palette, key } from 'styled-theme';
+
+import Tooltip from '../tooltip';
+
+const sizes = {
+  small: key('sizes.tooltip.small', '15px'),
+  normal: key('sizes.tooltip.normal', '30px'),
+};
+
+export const TooltipIconWrapper = styled(Tooltip)`
+  min-width: ${sizes.normal} !important;
+  width: ${sizes.normal};
+  height: ${sizes.normal};
+  border-radius: 50%;
+
+  &:hover {
+    box-shadow: 0px 0px 4px 2px ${palette('secondary', 2)};
+  }
+`;
