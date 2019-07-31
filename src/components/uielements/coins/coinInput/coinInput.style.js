a4c14096e63e971574eba7a320c7dccb8ad4b1e7
hectorstudio <david.liu987@hotmail.com>
2019-07-30 20:20:22 -0400

    Add coin input component style

--- src/components/uielements/coins/coinInput/coinInput.style.js
+++ src/components/uielements/coins/coinInput/coinInput.style.js
@@ -4,4 +4,24 @@ import { palette } from 'styled-theme';
 export const CoinInputWrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  .title-label,
+  .amount-label {
+  }
+
+  .coin-button {
+    min-width: 150px;
+    width: 150px;
+    margin-bottom: 20px;
+  }
+
+  .amount-wrapper {
+    display: flex;
+    align-items: center;
+
+    .asset-name-label {
+      margin-left: 4px;
+      text-transform: uppercase;
+    }
+  }
 `;
