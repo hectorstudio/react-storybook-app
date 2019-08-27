0b7fa31eaa3d7daf6622093aad13f06e20cd43ec
hectorstudio <david.liu987@hotmail.com>
2019-08-27 15:36:36 -0400

    Setup redux store with saga middleware

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
+import { store as reduxStore, history } from './redux/store';
 import PublicRoutes from './router';
 import AppHolder from './AppStyle';
 import { defaultTheme } from './settings';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <AppHolder>
-        <PublicRoutes />
-      </AppHolder>
-    </ThemeProvider>
+    <ReduxProvider store={reduxStore}>
+      <ThemeProvider theme={defaultTheme}>
+        <AppHolder>
+          <PublicRoutes history={history} />
+        </AppHolder>
+      </ThemeProvider>
+    </ReduxProvider>
   );
 }
 
