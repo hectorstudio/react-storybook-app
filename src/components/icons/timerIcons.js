9727d3f8d74ed39f752cff4d40c346d0cc5e0b87
hectorstudio <david.liu987@hotmail.com>
2019-08-31 05:40:20 -0400

    Add Timer Icons

---
+++ src/components/icons/timerIcons.js
@@ -0,0 +1,13 @@
+import { ReactComponent as TimerFullIcon } from '../../assets/timer/timerchart-full.svg';
+import { ReactComponent as TimerChartIcon1 } from '../../assets/timer/timerchart-1.svg';
+import { ReactComponent as TimerChartIcon2 } from '../../assets/timer/timerchart-2.svg';
+import { ReactComponent as TimerChartIcon3 } from '../../assets/timer/timerchart-3.svg';
+import { ReactComponent as TimerChartIcon4 } from '../../assets/timer/timerchart-4.svg';
+
+export {
+  TimerFullIcon,
+  TimerChartIcon1,
+  TimerChartIcon2,
+  TimerChartIcon3,
+  TimerChartIcon4,
+};
