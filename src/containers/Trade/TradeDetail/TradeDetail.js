939c6df4c80de41cf674c31b8e73cc4af6aca8e7
hectorstudio <david.liu987@hotmail.com>
2019-09-03 14:45:05 -0400

    Add trade confirmation modal with txtimer

--- src/containers/Trade/TradeDetail/TradeDetail.js
+++ src/containers/Trade/TradeDetail/TradeDetail.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Row, Col } from 'antd';
+import { Row, Col, Icon } from 'antd';
 
 import Drag from '../../../components/uielements/drag';
 import CoinCard from '../../../components/uielements/coins/coinCard';
 import Coin from '../../../components/uielements/coins/coin';
 import Status from '../../../components/uielements/status';
 import Slider from '../../../components/uielements/slider';
-import Modal from '../../../components/uielements/modal';
+import TxTimer from '../../../components/uielements/txTimer';
+import CoinData from '../../../components/uielements/coins/coinData';
+import Label from '../../../components/uielements/label';
+
 import { blackArrowIcon } from '../../../components/icons';
 
-import { ContentWrapper } from './TradeDetail.style';
+import {
+  ContentWrapper,
+  TradeModalContent,
+  TradeModal,
+} from './TradeDetail.style';
 
 import { getPair } from '../../../helpers/stringHelper';
 import { tradeData } from './data';
+
+import appActions from '../../../redux/app/actions';
+
+const {
+  setTxTimerType,
+  setTxTimerModal,
+  setTxTimerStatus,
+  setTxTimerValue,
+  resetTxStatus,
+} = appActions;
 
 class TradeDetail extends Component {
   static propTypes = {
     info: PropTypes.string,
     view: PropTypes.string.isRequired,
+    txStatus: PropTypes.object.isRequired,
+    setTxTimerType: PropTypes.func.isRequired,
+    setTxTimerModal: PropTypes.func.isRequired,
+    setTxTimerStatus: PropTypes.func.isRequired,
+    setTxTimerValue: PropTypes.func.isRequired,
+    resetTxStatus: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
@@ -28,7 +53,6 @@ class TradeDetail extends Component {
 
   state = {
     dragReset: true,
-    openConfirmModal: false,
   };
 
   handleDrag = () => {
@@ -38,9 +62,11 @@ class TradeDetail extends Component {
   };
 
   handleEndDrag = () => {
-    this.setState({
-      openConfirmModal: true,
-    });
+    const { setTxTimerModal, setTxTimerType, setTxTimerStatus } = this.props;
+
+    setTxTimerType('trade');
+    setTxTimerModal(true);
+    setTxTimerStatus(true);
   };
 
   handleConfirm = () => {
@@ -48,15 +74,93 @@ class TradeDetail extends Component {
   };
 
   handleCloseModal = () => {
+    const {
+      txStatus: { status },
+      setTxTimerModal,
+      resetTxStatus,
+    } = this.props;
+
+    if (!status) resetTxStatus();
+    else setTxTimerModal(false);
+  };
+
+  handleChangeTxValue = value => {
+    const { setTxTimerValue } = this.props;
+
+    setTxTimerValue(value);
+  };
+
+  handleEndTxTimer = () => {
+    const { setTxTimerStatus } = this.props;
+
+    setTxTimerStatus(false);
     this.setState({
       dragReset: true,
-      openConfirmModal: false,
     });
   };
 
+  renderTradeModalContent = pair => {
+    const {
+      txStatus: { status, value },
+    } = this.props;
+    const { source, target } = pair;
+
+    const transactionLabels = [
+      'sending transaction',
+      'processing transaction',
+      'signing transaction',
+      'finishing transaction',
+      'complete',
+    ];
+
+    const completed = value !== null && !status;
+    const tradeText = !completed ? 'YOU ARE TRADING' : 'YOU TRADED';
+    const receiveText = !completed ? 'YOU SHOULD RECEIVE' : 'YOU RECEIVED';
+    const expectation = !completed
+      ? 'EXPECTED FEES & SLIP'
+      : 'FINAL FEES & SLIP';
+
+    return (
+      <TradeModalContent>
+        <div className="left-container">
+          <Label weight="bold">{tradeText}</Label>
+          <CoinData asset={source} assetValue={2.49274} price={217.92} />
+        </div>
+        <div className="center-container">
+          <TxTimer
+            reset={status}
+            value={value}
+            onChange={this.handleChangeTxValue}
+            onEnd={this.handleEndTxTimer}
+          />
+          {value !== 0 && (
+            <Label weight="bold">{transactionLabels[value - 1]}</Label>
+          )}
+          {completed && <Label weight="bold">complete</Label>}
+        </div>
+        <div className="right-container">
+          <Label weight="bold">{receiveText}</Label>
+          <CoinData asset={target} assetValue={2.49274} price={217.92} />
+          <Label weight="bold">{expectation}</Label>
+          <div className="expected-status">
+            <div className="status-item">
+              <Status title="FEES" value="1.234 RUNE" />
+              <Label className="price-label" size="normal" color="gray">
+                $USD 110
+              </Label>
+            </div>
+            <div className="status-item">
+              <Status title="SLIP" value="0.3%" />
+            </div>
+          </div>
+        </div>
+      </TradeModalContent>
+    );
+  };
+
   render() {
-    const { info } = this.props;
-    const { dragReset, openConfirmModal } = this.state;
+    const { info, txStatus } = this.props;
+    const { dragReset } = this.state;
     const pair = getPair(info);
 
     if (!pair) {
@@ -67,6 +171,9 @@ class TradeDetail extends Component {
 
     const dragTitle = 'Drag to trade';
     const poolStatus = `${source}:${target}`;
+
+    const openTradeModal = txStatus.type === 'trade' ? txStatus.modal : false;
+    const coinCloseIconType = txStatus.status ? 'fullscreen-exit' : 'close';
 
     return (
       <ContentWrapper className="trade-detail-wrapper">
@@ -120,18 +227,34 @@ class TradeDetail extends Component {
             </div>
           </Col>
         </Row>
-        <Modal
-          title="Trade"
-          visible={openConfirmModal}
-          onOk={this.handleConfirm}
+        <TradeModal
+          title="TRADE CONFIRMATION"
+          closeIcon={
+            <Icon type={coinCloseIconType} style={{ color: '#33CCFF' }} />
+          }
+          visible={openTradeModal}
+          footer={null}
           onCancel={this.handleCloseModal}
-          okText="Confirm"
         >
-          <span>Do you want to Trade?</span>
-        </Modal>
+          {this.renderTradeModalContent(pair)}
+        </TradeModal>
       </ContentWrapper>
     );
   }
 }
 
-export default withRouter(TradeDetail);
+export default compose(
+  connect(
+    state => ({
+      txStatus: state.App.txStatus,
+    }),
+    {
+      setTxTimerType,
+      setTxTimerModal,
+      setTxTimerStatus,
+      setTxTimerValue,
+      resetTxStatus,
+    },
+  ),
+  withRouter,
+)(TradeDetail);
