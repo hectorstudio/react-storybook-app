3ba8d02c0a1b4f95e396471edb734e873dfecb6b
Chad Barraford <cbarraford@gmail.com>
2019-09-05 13:35:42 +0200

    get my stakes

--- src/env.js
+++ src/env.js
@@ -1,9 +1,11 @@
 const prod_hostnames = ['bepswap.com'];
+const dev_hostnames = ['localhost'];
 
 const isMainnet = prod_hostnames.includes(window.location.hostname);
 const isTestnet = !isMainnet;
+const isDevnet = dev_hostnames.includes(window.location.hostname);
 
 const NET = isTestnet ? 'testnet' : 'mainnet';
 const CHAIN_ID = isTestnet ? 'Binance-Chain-Nile' : 'Binance-Chain-Tigris';
 
-export { NET, CHAIN_ID, isTestnet, isMainnet };
+export { NET, CHAIN_ID, isDevnet, isTestnet, isMainnet };
