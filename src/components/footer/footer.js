0e9671fdef873acf9ae8aded206f0df7ea4f2a5f
Chad Barraford <cbarraford@gmail.com>
2019-09-07 14:08:08 +0200

    fix footer from overlapping content

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Icon } from 'antd';
+import { Icon, Layout } from 'antd';
 
 import Logo from '../uielements/logo';
 import { StyledFooter } from './footer.style';
@@ -8,50 +8,52 @@ import { StyledFooter } from './footer.style';
 class Footer extends Component {
   render() {
     return (
-      <StyledFooter>
-        <a
-          href="https://thorchain.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Logo className="footer-logo" name="thorchain" type="long" />
-        </a>
-        <div className="footer-links-bar">
-          <Link to="stats">STATS</Link>
-          <Link to="network">NETWORK</Link>
-          <Link to="faqs">FAQS</Link>
-        </div>
-        <div className="footer-social-bar">
+      <Layout.Footer style={{ padding: '24px 0px' }}>
+        <StyledFooter>
           <a
-            href="https://twitter.com/thorchain_org"
+            href="https://thorchain.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Icon type="twitter" />
+            <Logo className="footer-logo" name="thorchain" type="long" />
           </a>
-          <a
-            href="https://reddit.com/r/thorchain"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon type="reddit" />
-          </a>
-          <a
-            href="https://medium.com/thorchain"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon type="medium" />
-          </a>
-          <a
-            href="https://github.com/thorchain"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon type="github" />
-          </a>
-        </div>
-      </StyledFooter>
+          <div className="footer-links-bar">
+            <Link to="stats">STATS</Link>
+            <Link to="network">NETWORK</Link>
+            <Link to="faqs">FAQS</Link>
+          </div>
+          <div className="footer-social-bar">
+            <a
+              href="https://twitter.com/thorchain_org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon type="twitter" />
+            </a>
+            <a
+              href="https://reddit.com/r/thorchain"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon type="reddit" />
+            </a>
+            <a
+              href="https://medium.com/thorchain"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon type="medium" />
+            </a>
+            <a
+              href="https://github.com/thorchain"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon type="github" />
+            </a>
+          </div>
+        </StyledFooter>
+      </Layout.Footer>
     );
   }
 }
