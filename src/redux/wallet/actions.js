945d1770a9ba5d2bb923af2caf0f002df080b215
Chris Yu <luxtars.dev@gmail.com>
2019-09-08 16:43:06 -0400

    Add forget button and sync with states

--- src/redux/wallet/actions.js
+++ src/redux/wallet/actions.js
@@ -2,11 +2,17 @@ const actions = {
   CHECK_USER: 'CHECK_USER',
 
   SAVE_WALLET: 'SAVE_WALLET',
+  FORGET_WALLET: 'FORGET_WALLET',
 
   checkUser: () => ({ type: actions.CHECK_USER }),
 
   saveWallet: payload => ({
     type: actions.SAVE_WALLET,
+    payload,
+  }),
+
+  forgetWallet: payload => ({
+    type: actions.FORGET_WALLET,
     payload,
   }),
 };
