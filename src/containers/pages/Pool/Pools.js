4a3300f8c64b3e061dc6e985b268f7a8baaa9325
hectorstudio <david.liu987@hotmail.com>
2019-09-10 12:44:59 -0400

    Fix pool routing

--- src/containers/pages/Pool/Pools.js
+++ src/containers/pages/Pool/Pools.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
 
 import { PoolWrapper } from './Pool.style';
 import ActionView from '../../ActionView';
 
-class Pools extends Component {
+class Pool extends Component {
   render() {
     return (
       <PoolWrapper>
-        <ActionView type="pool" view={'view'} />
+        <ActionView type="pool" view="view" />
       </PoolWrapper>
     );
   }
 }
 
-export default withRouter(Pools);
+export default Pool;
