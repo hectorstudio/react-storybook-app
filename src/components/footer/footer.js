a5db2cb0708b61737c7b25320685bd53a70f1f76
Rudi Yardley <contact@rudiyardley.com>
2019-09-16 12:26:07 +0000

    Resolve "Make listing pages responsive to a minimum standard"

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,60 +1,66 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Icon, Layout } from 'antd';
+import { Icon } from 'antd';
 
 import Logo from '../uielements/logo';
-import { StyledFooter } from './footer.style';
+import { StyledFooter, FooterContainer, FooterItem } from './footer.style';
 
 class Footer extends Component {
   render() {
     return (
-      <Layout.Footer style={{ padding: '24px 0px' }}>
+      <FooterContainer>
         <StyledFooter>
-          <a
-            href="https://thorchain.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Logo className="footer-logo" name="thorchain" type="long" />
-          </a>
-          <div className="footer-links-bar">
-            <Link to="/stats">STATS</Link>
-            <Link to="/network">NETWORK</Link>
-            <Link to="/tutorial">TUTORIAL</Link>
-            <Link to="/faqs">FAQS</Link>
-          </div>
-          <div className="footer-social-bar">
+          <FooterItem>
             <a
-              href="https://twitter.com/thorchain_org"
+              href="https://thorchain.org"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Icon type="twitter" />
+              <Logo className="footer-logo" name="thorchain" type="long" />
             </a>
-            <a
-              href="https://reddit.com/r/thorchain"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Icon type="reddit" />
-            </a>
-            <a
-              href="https://medium.com/thorchain"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Icon type="medium" />
-            </a>
-            <a
-              href="https://github.com/thorchain"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Icon type="github" />
-            </a>
-          </div>
+          </FooterItem>
+          <FooterItem>
+            <div className="footer-links-bar">
+              <Link to="/stats">STATS</Link>
+              <Link to="/network">NETWORK</Link>
+              <Link to="/tutorial">TUTORIAL</Link>
+              <Link to="/faqs">FAQS</Link>
+            </div>
+          </FooterItem>
+          <FooterItem>
+            <div className="footer-social-bar">
+              <a
+                href="https://twitter.com/thorchain_org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon type="twitter" />
+              </a>
+              <a
+                href="https://reddit.com/r/thorchain"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon type="reddit" />
+              </a>
+              <a
+                href="https://medium.com/thorchain"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon type="medium" />
+              </a>
+              <a
+                href="https://github.com/thorchain"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon type="github" />
+              </a>
+            </div>
+          </FooterItem>
         </StyledFooter>
-      </Layout.Footer>
+      </FooterContainer>
     );
   }
 }
