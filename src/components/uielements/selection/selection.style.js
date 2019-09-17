af4c08acb33f0dc8ef171c88b4589f1f43b27179
Rudi Yardley <contact@rudiyardley.com>
2019-09-17 20:02:28 +0000

    Fix for button bugs #113 #112

--- src/components/uielements/selection/selection.style.js
+++ src/components/uielements/selection/selection.style.js
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 import { palette } from 'styled-theme';
+import UIButton from '../button';
 
 export const SelectionWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 200px;
+`;
 
-  button {
-    min-width: 45px !important;
-    width: 45px;
-    border-color: ${palette('border', 0)} !important;
-    color: ${palette('border', 0)} !important;
-  }
+export const Button = styled(UIButton)`
+  min-width: 45px;
+  width: 45px;
+  /* TODO: create specific style for muted buttons to avoid using !important */
+  border-color: ${palette('border', 0)} !important;
+  color: ${palette('border', 0)} !important;
 `;
