62a090d5ce2bdeaade411284055205284b550b9a
Chris Yu <luxtars.dev@gmail.com>
2019-10-07 02:43:27 -0400

    Remove mock adapter from statechain client

--- src/clients/statechain.js
+++ src/clients/statechain.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import querystring from 'querystring';
 
-import { NET, isDevnet, isTestnet } from '../env';
+import { NET, isTestnet } from '../env';
 
 class StateChain {
   constructor() {
@@ -18,65 +16,6 @@ class StateChain {
       baseURL: this.baseURL,
       contentType: 'application/json',
     });
-
-    if (isDevnet) {
-      console.log('Using mock chain service');
-      const mock = new MockAdapter(this.httpClient);
-
-      mock.onAny().reply(config => {
-        const url = new URL(config.url);
-        const path = url.pathname;
-        const q = querystring.parse(url.search.slice(1));
-        console.log(path, q);
-
-        if (path === '/swapservice/ping') {
-          return [200, { ping: 'pong' }];
-        } else if (path === '/swapservice/pools') {
-          return [
-            200,
-            [
-              {
-                balance_rune: 67.36,
-                balance_token: 23.539,
-                symbol: 'BNB',
-                pool_units: 23.6,
-                pool_status: 'Enabled',
-              },
-              {
-                balance_rune: 765.524,
-                balance_token: 1756.879,
-                symbol: 'TOMO',
-                pool_units: 7562.77,
-                pool_status: 'Suspended',
-              },
-              {
-                balance_rune: 658.61,
-                balance_token: 137.876,
-                symbol: 'BOLT',
-                pool_units: 34.68,
-                pool_status: 'Enabled',
-              },
-              {
-                balance_rune: 658.61,
-                balance_token: 137.876,
-                symbol: 'FTM',
-                pool_units: 34.68,
-                pool_status: 'Enabled',
-              },
-              {
-                balance_rune: 658.61,
-                balance_token: 137.876,
-                symbol: 'ANKR',
-                pool_units: 34.68,
-                pool_status: 'Enabled',
-              },
-            ],
-          ];
-        } else {
-          return [404, 'mock not implemented'];
-        }
-      });
-    }
   }
 
   healthcheck() {
