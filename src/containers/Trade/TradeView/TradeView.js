2079ef9a9fb7099079f9f3914dcc7e862f536a1e
Chris Yu <luxtars.dev@gmail.com>
2019-10-08 13:54:19 -0400

    Implement trade list view page

--- src/containers/Trade/TradeView/TradeView.js
+++ src/containers/Trade/TradeView/TradeView.js
@@ -1,15 +1,40 @@
 import React, { Fragment, Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import TradeCard from '../../../components/trade/tradeCard';
 
+import TradeCard from '../../../components/trade/tradeCard';
 import { ContentWrapper } from './TradeView.style';
-import { assets } from './data';
+import { getTradeData } from '../utils';
+
+import statechainActions from '../../../redux/statechain/actions';
+import walletactions from '../../../redux/wallet/actions';
+
+const { getPools } = statechainActions;
+const { getRunePrice } = walletactions;
 
 class TradeView extends Component {
-  static propTypes = { history: PropTypes.object };
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+    getPools: PropTypes.func.isRequired,
+    pools: PropTypes.array.isRequired,
+    poolData: PropTypes.object.isRequired,
+    swapData: PropTypes.object.isRequired,
+    assetData: PropTypes.array.isRequired,
+    getRunePrice: PropTypes.func.isRequired,
+    runePrice: PropTypes.number.isRequired,
+    loading: PropTypes.bool.isRequired,
+  };
 
   state = {};
+
+  componentDidMount() {
+    const { getPools, getRunePrice } = this.props;
+
+    getPools();
+    getRunePrice();
+  }
 
   handleTrade = asset => () => {
     const URL = `/trade/${asset}`;
@@ -18,19 +43,34 @@ class TradeView extends Component {
   };
 
   renderTradeList = () => {
-    return assets.map((asset, index) => {
-      if (asset !== 'bnb') {
+    const { pools, poolData, swapData, runePrice, assetData } = this.props;
+
+    return pools.map((pool, index) => {
+      const { symbol } = pool;
+      const poolInfo = poolData[symbol] || {};
+      const swapInfo = swapData[symbol] || {};
+
+      const { target, depth } = getTradeData(
+        'rune',
+        symbol,
+        poolInfo,
+        swapInfo,
+        assetData,
+        runePrice,
+      );
+
+      if (target !== 'bnb') {
         return (
           <TradeCard
             className="trade-card"
             asset="bnb"
-            target={asset}
-            depth={23000}
+            target={target}
+            depth={depth}
             poolPrice={1.2}
             marketPrice={1}
             premium={20}
             reward={230}
-            onTrade={this.handleTrade(asset)}
+            onTrade={this.handleTrade(symbol)}
             key={index}
           />
         );
@@ -48,4 +88,20 @@ class TradeView extends Component {
   }
 }
 
-export default withRouter(TradeView);
+export default compose(
+  connect(
+    state => ({
+      pools: state.Statechain.pools,
+      poolData: state.Statechain.poolData,
+      swapData: state.Statechain.swapData,
+      loading: state.Statechain.loading,
+      runePrice: state.Wallet.runePrice,
+      assetData: state.Wallet.assetData,
+    }),
+    {
+      getPools,
+      getRunePrice,
+    },
+  ),
+  withRouter,
+)(TradeView);
