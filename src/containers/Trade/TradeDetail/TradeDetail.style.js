17dfc3531d2691d2e2d46829290eb2a21de0f7bc
Chris Yu <luxtars.dev@gmail.com>
2019-10-11 08:53:16 -0400

    Add trade card view style

--- src/containers/Trade/TradeDetail/TradeDetail.style.js
+++ src/containers/Trade/TradeDetail/TradeDetail.style.js
@@ -29,41 +29,26 @@ export const ContentWrapper = styled(ContentView)`
     }
   }
 
-  .trade-detail-row {
-    flex-grow: 1;
+  .trade-values {
+  }
 
-    .trade-detail-panel {
+  .trade-panel {
+    .ant-col {
       display: flex;
-      flex-direction: column;
-      padding: 10px 20px !important;
-      border-right: 1px solid ${palette('border', 0)};
+      justify-content: space-around;
+      align-items: center;
 
-      .trade-asset-card-wrapper {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+      &:first-child {
+        border-right: 1px solid ${palette('border', 0)};
+      }
 
-        .trade-asset-card {
-          height: 150px;
-          .trade-asset-slider {
-            padding: 10px 0;
-          }
+      .trade-card {
+        .slider-wrapper {
+          margin: 20px 0;
         }
       }
-      .drag-confirm-wrapper {
-        display: flex;
-        justify-content: center;
-        padding-top: 40px;
-      }
-    }
-
-    .trade-info-wrapper {
-      display: flex;
-      flex-wrap: wrap;
-      padding: 10px 20px;
-
-      .status-wrapper {
-        width: 50%;
+      .trade-btn {
+        margin: 0 40px;
       }
     }
   }
