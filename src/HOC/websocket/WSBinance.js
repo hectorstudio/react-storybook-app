0d001c934fe2a1233b5c2e73dd3a5db4b1517a32
hectorstudio <david.liu987@hotmail.com>
2019-10-14 03:10:39 -0400

    Use API URIs from dotenv

--- src/HOC/websocket/WSBinance.js
+++ src/HOC/websocket/WSBinance.js
@@ -1,11 +1,11 @@
 import { getWalletAddress } from '../../helpers/webStorageHelper';
 import withWebsocket from './withWebsocket';
 
-const BINANCE_WEBSOCKET_URI = 'wss://dex.binance.org/api/ws';
+const BINANCE_MAINNET_WS_URI = process.env.BINANCE_MAINNET_WS_URI;
 
 export const withBinanceWS = () => {
   const address = getWalletAddress();
-  const url = `${BINANCE_WEBSOCKET_URI}/${address}`;
+  const url = `${BINANCE_MAINNET_WS_URI}/${address}`;
 
   return withWebsocket(url, 'binanceWS');
 };
