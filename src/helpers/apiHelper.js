ec58eba7083b3c376f43d4bc281e551bae82d557
hectorstudio <david.liu987@hotmail.com>
2019-10-14 13:29:18 -0400

    Update env vars with prefix

--- src/helpers/apiHelper.js
+++ src/helpers/apiHelper.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
+export const COINGECKO_API_URL = process.env.REACT_APP_COINGECKO_API_URL;
 export const CHAINSERVICE_BASE_URL = process.env.REACT_APP_CHAINSERVICE_API_URL;
 export const STATECHAIN_BASE_URL = process.env.REACT_APP_STATECHAIN_API_URL;
-export const COINGECKO_BASE_URL = process.env.COINGECKO_BASE_URL;
-export const BINANCE_TESTNET_URL = process.env.BINANCE_TESTNET_URL;
-export const BINANCE_MAINNET_URL = process.env.BINANCE_MAINNET_URL;
+export const BINANCE_TESTNET_URL = process.env.REACT_APP_BINANCE_TESTNET_URL;
+export const BINANCE_MAINNET_URL = process.env.REACT_APP_BINANCE_MAINNET_URL;
 export const TESTNET_TX_BASE_URL = 'https://testnet-explorer.binance.org/tx/';
 
+console.log('coingecko url ', COINGECKO_API_URL);
 export const getChainserviceURL = url => `${CHAINSERVICE_BASE_URL}/${url}`;
 export const getStatechainURL = url => `${STATECHAIN_BASE_URL}/${url}`;
-export const getCoinGeckoURL = url => `${COINGECKO_BASE_URL}/${url}`;
+export const getCoinGeckoURL = url => `${COINGECKO_API_URL}/${url}`;
 export const getBinanceTestnetURL = url => `${BINANCE_TESTNET_URL}/${url}`;
 export const getBinanceMainnetURL = url => `${BINANCE_MAINNET_URL}/${url}`;
 
