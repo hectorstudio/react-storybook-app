2e027a456ebfd34d839e8d31dee365cb682b7a36
Chris Yu <luxtars.dev@gmail.com>
2019-10-23 18:04:19 -0400

    Add trend component

---
+++ src/components/uielements/trend/trend.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Icon } from 'antd';
+
+import Label from '../label';
+
+import { getFixedNumber } from '../../../helpers/stringHelper';
+
+import { TrendWrapper } from './trend.style';
+
+class Trend extends Component {
+  static propTypes = {
+    value: PropTypes.number.isRequired,
+  };
+
+  render() {
+    const { value, ...otherProps } = this.props;
+    const trend = value >= 0;
+    const trendIcon = trend ? 'arrow-up' : 'arrow-down';
+    const trendVal = `${getFixedNumber(Math.abs(value))}%`;
+
+    return (
+      <TrendWrapper trend={trend} {...otherProps}>
+        <Icon type={trendIcon} />
+        <Label>{trendVal}</Label>
+      </TrendWrapper>
+    );
+  }
+}
+
+export default Trend;
