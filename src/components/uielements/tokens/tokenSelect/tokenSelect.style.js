9b9b3fdc7d9aec2a97bdb30bc83320aeb164fc78
hectorstudio <david.liu987@hotmail.com>
2019-10-24 12:27:28 -0400

    Update style for tokenSelect

--- src/components/uielements/tokens/tokenSelect/tokenSelect.style.js
+++ src/components/uielements/tokens/tokenSelect/tokenSelect.style.js
@@ -1,17 +1,63 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { palette } from 'styled-theme';
+import { Icon } from 'antd';
 import { transition } from '../../../../settings/style-util';
+import { cleanTag } from '../../../../helpers/styleHelper';
 
 export const TokenSelectWrapper = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
 
-  width: 196px;
+  width: 216px;
   height: 60px;
   border: 1px solid ${palette('border', 0)};
   border-radius: 2px;
   text-transform: uppercase;
   ${transition()};
+`;
+
+const IconBase = cleanTag(Icon, ['open']);
+export const DropdownIcon = styled(IconBase)`
+  transition: transform 0.2s ease-in-out;
+  ${({ open }) =>
+    open ? 'transform: rotate(180deg);' : 'transform: rotate(0);'}
+  font-size: 18px;
+
+  &.caret-down {
+    font-size: 22px;
+    color: ${palette('primary', 0)};
+  }
+`;
+
+export const DropdownIconHolder = styled.div`
+  transition: transform 0.2s ease-in-out;
+  padding-top: 5px;
+`;
+
+export const TokenDropdownButton = styled.button`
+  ${({ disabled }) => css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    &:focus {
+      outline: none;
+    }
+    > * {
+      margin-right: 10px;
+    }
+
+    ${!disabled
+      ? css`
+          &:hover {
+            ${DropdownIconHolder} {
+              transform: translateY(-1px);
+            }
+          }
+        `
+      : ''};
+  `}
 `;
