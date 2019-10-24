07977fb04f60889c1f083b5251b96cedf1970105
hectorstudio <david.liu987@hotmail.com>
2019-10-24 13:50:22 -0400

    Update addIcon component

--- src/components/uielements/addIcon/addIcon.style.js
+++ src/components/uielements/addIcon/addIcon.style.js
@@ -2,23 +2,22 @@ import styled from 'styled-components';
 import { palette } from 'styled-theme';
 
 export const IconWrapper = styled.div`
-  width: 30px;
-  height: 30px;
+  width: 20px;
+  height: 20px;
   border: none;
   border-radius: 50%;
-  background: #fff;
-  color: ${palette('primary', 0)};
-  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  color: #fff;
+  background: ${palette('primary', 1)};
   cursor: pointer;
 
   span {
     position: relative;
-    top: -2px;
-    left: 8px;
-    font-size: 24px;
+    top: -6px;
+    left: 4px;
+    font-size: 20px;
     &::selection {
       background-color: transparent;
-      color: ${palette('primary', 0)};
+      color: #fff;
     }
   }
 `;
