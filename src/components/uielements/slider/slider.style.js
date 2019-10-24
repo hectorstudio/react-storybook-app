01ace2ebfa090926391c55cb4bfc2f5e89ca779d
Chris Yu <luxtars.dev@gmail.com>
2019-10-24 10:42:47 -0400

    Change slider style

--- src/components/uielements/slider/slider.style.js
+++ src/components/uielements/slider/slider.style.js
@@ -6,13 +6,8 @@ export const SliderWrapper = styled(Slider)`
   &.ant-slider {
     margin-bottom: 25px;
     .ant-slider-rail {
-      height: 5px;
-      background: linear-gradient(
-        to right,
-        ${palette('primary', 5)} 0%,
-        ${palette('primary', 1)} 50%,
-        ${palette('primary', 5)} 100%
-      );
+      height: 4px;
+      background: ${palette('primary', 0)};
     }
 
     .ant-slider-track {
@@ -20,18 +15,12 @@ export const SliderWrapper = styled(Slider)`
     }
 
     .ant-slider-handle {
-      width: 30px;
-      height: 30px;
-      margin-top: -12px;
-      margin-left: -15px;
-      border: none;
-      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
-    }
-
-    &:hover {
-      .ant-slider-track {
-        background: ${palette('primary', 1)};
-      }
+      width: 14px;
+      height: 14px;
+      margin-top: -6px;
+      margin-left: -6px;
+      border: 3px solid ${palette('success', 0)};
+      background: #fff;
     }
   }
 `;
