f08a572ddc0edcda6bfde695f0b4d2b8e56580a1
Chris Yu <luxtars.dev@gmail.com>
2019-10-24 12:03:16 -0400

    Build tokenData component

---
+++ src/components/uielements/tokens/tokenData/tokenData.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { TokenDataWrapper } from './tokenData.style';
+
+import Coin from '../../coins/coin';
+import Label from '../../label';
+
+class TokenData extends Component {
+  static propTypes = {
+    asset: PropTypes.string,
+    assetValue: PropTypes.number,
+    price: PropTypes.number,
+    className: PropTypes.string,
+  };
+
+  static defaultProps = {
+    asset: 'bnb',
+    assetValue: null,
+    price: 0,
+    className: '',
+  };
+
+  render() {
+    const { asset, assetValue, price, className, ...props } = this.props;
+
+    return (
+      <TokenDataWrapper
+        assetValue={assetValue}
+        className={`coinData-wrapper ${className}`}
+        {...props}
+      >
+        <Coin className="coinData-coin-avatar" type={asset} />
+        <div className="coinData-asset-label">{asset}</div>
+        <div className="asset-price-info">
+          {`$${Number(price.toFixed(2)).toLocaleString()}`}
+        </div>
+      </TokenDataWrapper>
+    );
+  }
+}
+
+export default TokenData;
