b81e2b27fe801742915853c96f74a47c097fc3b0
Chris Yu <luxtars.dev@gmail.com>
2019-11-14 22:19:17 +0000

    Resolve "Update confirmation modal"

---
+++ src/components/uielements/oldTxTimer/txTimer.style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const TxTimerWrapper = styled.div`
+  position: relative;
+  width: 100px;
+  height: 100px;
+
+  .timerchart-icon {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    height: 100%;
+
+    .confirm-icon {
+    }
+  }
+
+  .timerchart-circular-progressbar {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+
+    &.hide {
+      visibility: hidden;
+    }
+  }
+`;
