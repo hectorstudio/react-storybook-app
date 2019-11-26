bf66bcf2b807ae6ddfb5b40080b382f2677d120e
Chris Yu <luxtars.dev@gmail.com>
2019-11-26 10:12:28 -0500

    Add midgard api to env and apihelper

--- src/helpers/apiHelper.js
+++ src/helpers/apiHelper.js
@@ -5,6 +5,7 @@ export const CHAINSERVICE_BASE_URL = process.env.REACT_APP_CHAINSERVICE_API_URL;
 export const STATECHAIN_BASE_URL = process.env.REACT_APP_STATECHAIN_API_URL;
 export const BINANCE_TESTNET_URL = process.env.REACT_APP_BINANCE_TESTNET_URL;
 export const BINANCE_MAINNET_URL = process.env.REACT_APP_BINANCE_MAINNET_URL;
+export const MIDGARD_API_URL = process.env.REACT_APP_MIDGARD_API_URL;
 export const TESTNET_TX_BASE_URL = 'https://testnet-explorer.binance.org/tx/';
 
 console.log('coingecko url ', COINGECKO_API_URL);
@@ -13,6 +14,7 @@ export const getStatechainURL = url => `${STATECHAIN_BASE_URL}/${url}`;
 export const getCoinGeckoURL = url => `${COINGECKO_API_URL}/${url}`;
 export const getBinanceTestnetURL = url => `${BINANCE_TESTNET_URL}/${url}`;
 export const getBinanceMainnetURL = url => `${BINANCE_MAINNET_URL}/${url}`;
+export const getMidgardURL = url => `${MIDGARD_API_URL}/v1/${url}`;
 
 const defaultAxios = axios.create();
 
