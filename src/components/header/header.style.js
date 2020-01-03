88020b462bba16318e8da3e3f3fb1f82a9b280f8
Chris Yu <luxtars.dev@gmail.com>
2020-01-03 11:06:10 +0000

    Resolve "Update base price selector in the header"

--- src/components/header/header.style.js
+++ src/components/header/header.style.js
@@ -139,5 +139,18 @@ export const HeaderActionButtons = styled.div`
     ${media.sm`
       display: flex;
     `}
+
+    .currency-icon-container {
+      svg {
+        width: 20px;
+        height: 20px;
+        margin-top: 6px;
+        margin-right: 4px;
+      }
+    }
+
+    .label-wrapper {
+      text-transform: uppercase;
+    }
   }
 `;
