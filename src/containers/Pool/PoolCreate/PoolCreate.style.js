8617970fb62137af268bd928b0bf0d1151069537
hectorstudio <david.liu987@hotmail.com>
2020-01-12 07:44:34 +0000

    Resolve "FIX: Staking modal doesn't appear when adding pool"

--- src/containers/Pool/PoolCreate/PoolCreate.style.js
+++ src/containers/Pool/PoolCreate/PoolCreate.style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { palette } from 'styled-theme';
 import ContentView from '../../../components/utility/contentView';
 import Modal from '../../../components/uielements/modal';
+import { media } from '../../../helpers/styleHelper';
 
 export const ContentWrapper = styled(ContentView)`
   padding: 0;
@@ -118,6 +119,110 @@ export const PrivateModal = styled(Modal)`
   .ant-modal-body {
     .ant-form-item {
       margin-bottom: 0;
+    }
+  }
+`;
+
+export const ConfirmModal = styled(Modal)`
+  &.ant-modal {
+    width: 420px !important;
+
+    .ant-modal-body {
+      padding: 0px;
+    }
+  }
+`;
+
+export const ConfirmModalContent = styled.div`
+  ${media.lg`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `}
+
+  .modal-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    padding: 30px 0;
+    border-bottom: 1px solid ${palette('border', 0)};
+
+    .coinData-wrapper {
+      padding-left: 0;
+      padding-bottom: 4px;
+      margin-left: 14px;
+    }
+
+    .status-wrapper {
+      .status-title {
+        ${media.lg`
+        padding-top: 0;
+      `}
+      }
+      .status-value {
+        ${media.lg`
+        padding-bottom: 0;
+      `}
+      }
+    }
+
+    .timer-container {
+      ${media.lg`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding-bottom: 30px;
+        `}
+    }
+
+    .coin-data-wrapper {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .coin-data-container {
+        display: flex;
+        flex-direction: column;
+
+        .coinData-wrapper {
+          &:first-child {
+            padding-bottom: 20px;
+          }
+        }
+      }
+    }
+  }
+
+  .modal-info-wrapper {
+    padding: 20px 0;
+
+    .hash-address {
+      display: flex;
+      align-items: center;
+
+      .copy-btn-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        border: 1px solid ${palette('primary', 0)};
+        border-radius: 6px;
+        padding: 1px 4px;
+        margin-right: 6px;
+        color: ${palette('primary', 0)};
+        cursor: pointer;
+
+        .view-btn {
+          width: 300px;
+          height: 40px;
+          margin: 24px 0;
+        }
+      }
+
+      .label-wrapper {
+        width: 100%;
+      }
     }
   }
 `;
